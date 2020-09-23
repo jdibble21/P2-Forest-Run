d365e4f77ffc111c9f8ea0820125762e5e3511c1
Jake Dibble <jdibble@bsu.edu>
2020-09-23 14:54:21 -0400

    Add Enemy patrol path

--- project/src/Enemy.gd
+++ project/src/Enemy.gd
@@ -1,39 +1,27 @@
 extends KinematicBody2D
 
-const SPEED = 100
+const SPEED = 80
 const GRAVITY = 30
+var _move_direction = 0
+var elapsed = 0
 var _velocity = Vector2(0,0)
 onready var _animation_control = $AnimatedSprite
+onready var _path_follow = get_parent()
 
-
-func _ready():
-	_act()
-	pass
+func _process(delta):
+	_patrol(delta)
 	
-func _act():
-	_patrol_left()
-	print("moving left...")
-	move_and_slide(_velocity)
-	
-	var timer = Timer.new()
-	timer.set_wait_time(5)
-	add_child(timer)
-	timer.start()
-	yield(timer, "timeout")
-
-	_patrol_right()
-	print("moving right...")
-	move_and_slide(_velocity)
-	
-	timer = Timer.new()
-	timer.set_wait_time(5)
-	
-	add_child(timer)
-	timer.start()
-	yield(timer, "timeout")
-	
-func _patrol_left():
-	_velocity.x = -SPEED
-	
-func _patrol_right():
-	_velocity.x = SPEED
+func _patrol(delta):
+	_animation_control.play("patrol")
+	elapsed = elapsed + delta
+	print(elapsed)
+	if elapsed < 5.0: 
+		_velocity.x = SPEED
+		_animation_control.flip_h = true
+	elif elapsed >= 5.0:
+		_velocity.x = -SPEED
+		_animation_control.flip_h = false
+	if elapsed >= 10.0:
+		elapsed = 0
+	_velocity = move_and_slide(_velocity)
+	_velocity.x = lerp(_velocity.x,0,0.5)
