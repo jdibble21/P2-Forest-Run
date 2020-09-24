c902bb252cc4608347253db8fa25f0c6b2f9eb33
Jake Dibble <jdibble@bsu.edu>
2020-09-24 16:27:40 -0400

    Add sword pickup

--- project/src/Player.gd
+++ project/src/Player.gd
@@ -6,45 +6,43 @@ const SPEED = 180
 const GRAVITY = 30
 const JUMPFORCE = -550
 var _velocity = Vector2(0,0)
-var _has_sword = false
+onready var has_sword
 onready var _node_enemy_1 = get_node("/root/World/Enemy1")
 onready var _animation_control = $AnimatedSprite
 
 func _ready():
-	_animation_control.play("idle")
+	has_sword = false
+	_play_idle_animation()
 	_node_enemy_1.connect("_hit_player", self, "_on_player_hit")
 	
 func _physics_process(delta):
 	if is_on_floor() and (!Input.is_action_pressed("move_left") and (!Input.is_action_pressed("move_right"))):
-		_animation_control.play("idle")
+		_play_idle_animation()
 	if Input.is_action_pressed("move_right"):
 		_velocity.x = SPEED 
 		_animation_control.play("run")
 		_animation_control.flip_h = false
 	elif Input.is_action_just_released("move_right"):
-		_animation_control.play("idle")
+		_play_idle_animation()
 	if Input.is_action_pressed("move_left"):
 		_velocity.x = -SPEED
 		_animation_control.flip_h = _velocity.x < 0
 		_animation_control.play("run")
 	elif Input.is_action_just_released("move_left"):
-		_animation_control.play("idle")
+		_play_idle_animation()
 	_velocity.y = _velocity.y + GRAVITY
 	if not is_on_floor():
 		_animation_control.play("jump")
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		_velocity.y = JUMPFORCE
-		
 	_velocity = move_and_slide(_velocity,Vector2.UP)
 	_velocity.x = lerp(_velocity.x,0,0.5)
 
 
 func _play_idle_animation():
-	if _has_sword:
-		pass
-		#play sword idle
-	if !(_has_sword):
-		pass
+	if has_sword:
+		_animation_control.play("idle_sword")
+	if !(has_sword):
 		_animation_control.play("idle")
 
 
--- project/src/World.gd
+++ project/src/World.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
 onready var _sword_pickup_animation = $CollectableSword/AnimatedSprite
+onready var _sword_pickup = $CollectableSword
 
 func _ready():
 	_sword_pickup_animation.play("standby")
 
+
+func _on_Area2D_area_entered(area):
+	_sword_pickup.hide()
+	$Player.has_sword = true
+	
